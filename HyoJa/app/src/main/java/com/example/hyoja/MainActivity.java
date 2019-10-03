a848ad858304ceada8b8e4dfd4f5b6a9171317e1
eunjuC1215 <eunju1215@kookmin.ac.kr>
2019-10-03 12:54:04 +0900

    QR코드 인식

--- HyoJa/app/src/main/java/com/example/hyoja/MainActivity.java
+++ HyoJa/app/src/main/java/com/example/hyoja/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.hyoja;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scanQRBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanQRBtn = (Button) findViewById(R.id.scanQR);
+
+        scanQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ScanQR.class);
+                startActivity(intent);
+            }
+        });
     }
 }
