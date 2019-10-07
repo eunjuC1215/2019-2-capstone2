89b354d194147052430c1f06dbb7b84a2c83dc63
eomjeongho <eomjeongho1@naver.com>
2019-10-07 15:05:17 +0900

    종정시 이용하여 로그인 구현

--- HyoJa/app/src/main/java/com/example/hyoja/LoginActivity.java
+++ HyoJa/app/src/main/java/com/example/hyoja/LoginActivity.java
@@ -1,29 +1,48 @@
 package com.example.hyoja;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText EditText_id, EditText_pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView loginButton = (TextView)findViewById(R.id.loginButton);
+        EditText_id = findViewById(R.id.idText);
+        EditText_pw = findViewById(R.id.passwordText);
+
+        Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(loginIntent);
+                Check check = new Check();
+                check.execute(EditText_id.getText().toString(), EditText_pw.getText().toString());
             }
         });
 
-        TextView registerButton = (TextView)findViewById(R.id.registerButton);
+        TextView registerButton = findViewById(R.id.registerButton);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +51,80 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(registerIntent);
             }
         });
-
     }
+
+    private class Check extends AsyncTask<String, Void, String>{
+        ProgressDialog progressDialogl;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialogl = ProgressDialog.show(LoginActivity.this, "Please wait...", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            progressDialogl.dismiss();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            if(s.equals("Success")){
+                startActivity(intent);
+            }else{
+                Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            String id = params[0];
+            String pw = params[1];
+
+            String server_url = "http://13.124.28.135/check.php";
+            String postParameters = "id=" + id + "&pw=" + pw;
+
+            try{
+                URL url = new URL(server_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+
+                InputStream inputStream;
+                if(responseStatusCode == httpURLConnection.HTTP_OK){
+                    inputStream = httpURLConnection.getInputStream();
+                }else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+            }catch (Exception e){
+                errorString = e.toString();
+                return null;
+            }
+        }
+    }
+
 }
