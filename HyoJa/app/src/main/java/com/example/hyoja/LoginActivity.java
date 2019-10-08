d29ddb27379f051414961ac70195ff55aafc4a1a
eomjeongho <eomjeongho1@naver.com>
2019-10-08 20:50:55 +0900

    자동로그인 구현

--- HyoJa/app/src/main/java/com/example/hyoja/LoginActivity.java
+++ HyoJa/app/src/main/java/com/example/hyoja/LoginActivity.java
@@ -1,28 +1,31 @@
 package com.example.hyoja;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
-
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText EditText_id, EditText_pw;
+    private CheckBox autoLogin;
+    String loginID, loginPW;
+    Boolean autologin_check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +34,31 @@ public class LoginActivity extends AppCompatActivity {
 
         EditText_id = findViewById(R.id.idText);
         EditText_pw = findViewById(R.id.passwordText);
+        autoLogin = findViewById(R.id.autoLogin);
+
+        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+
+        loginID = auto.getString("ID", null);
+        loginPW = auto.getString("PW", null);
+
+        if(loginID != null && loginPW != null){
+            EditText_id.setText(loginID);
+            EditText_pw.setText(loginPW);
+
+            Check check = new Check();
+            check.execute(EditText_id.getText().toString(), EditText_pw.getText().toString());
+        }
 
         Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(autoLogin.isChecked()){
+                    autologin_check = true;
+                }else{
+                    autologin_check = false;
+                }
                 Check check = new Check();
                 check.execute(EditText_id.getText().toString(), EditText_pw.getText().toString());
             }
@@ -54,22 +76,32 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private class Check extends AsyncTask<String, Void, String>{
-        ProgressDialog progressDialogl;
+        ProgressDialog progressDialog;
         String errorString = null;
 
         @Override
         protected void onPreExecute(){
             super.onPreExecute();
-            progressDialogl = ProgressDialog.show(LoginActivity.this, "Please wait...", null, true, true);
+            progressDialog = ProgressDialog.show(LoginActivity.this, "Please wait...", null, true, true);
         }
 
         @Override
-        protected void onPostExecute(String s){
+        protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            progressDialogl.dismiss();
+            progressDialog.dismiss();
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
             if(s.equals("Success")){
+                if(loginID == null && loginPW == null && autologin_check){
+                    SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = auto.edit();
+
+                    editor.putString("ID", EditText_id.getText().toString());
+                    editor.putString("PW", EditText_pw.getText().toString());
+                    editor.commit();
+                }
                 startActivity(intent);
+                Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_LONG).show();
             }else{
                 Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_LONG).show();
             }
--- HyoJa/app/src/main/java/com/example/hyoja/MainActivity.java
+++ HyoJa/app/src/main/java/com/example/hyoja/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.hyoja;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button scanQRBtn;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, ScanQR.class);
                 startActivity(intent);
+            }
+        });
+
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = auto.edit();
+
+                editor.clear();
+                editor.commit();
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
