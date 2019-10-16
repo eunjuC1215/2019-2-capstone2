9d056de7fe60826934dfd41a51342c7112f4a1c3
eomjeongho <eomjeongho1@naver.com>
2019-10-16 18:11:15 +0900

    DB연동 및 좌석 선택 구현

--- HyoJa/app/src/main/java/com/example/hyoja/ReserveActivity.java
+++ HyoJa/app/src/main/java/com/example/hyoja/ReserveActivity.java
@@ -1,13 +1,262 @@
 package com.example.hyoja;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ReserveActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReserveActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ViewGroup layout;
+
+    public static String seats = "000_00_000/"
+            +"000_00_000/"
+            +"____00____/"
+            +"____00____/"
+            +"000_00_000/"
+            +"000_00_000/"
+            +"____00____/"
+            +"____00____/"
+            +"000____000/"
+            +"000____000/"
+            +"____00____/"
+            +"____00____/"
+            +"000_00_000/"
+            +"000_00_000/"
+            +"____00____/"
+            +"____00____/"
+            +"000_00_000/"
+            +"000_00_000/";
+
+    List<TextView> seatViewList = new ArrayList<>();
+    int seatSize = 100;
+    int seatGaping = 10;
+
+    int STATUS_AVAILABLE = 0;
+    int STATUS_BOOKED = 1;
+    int STATUS_RESERVED = 2;
+    String selectedIds = "";
+
+    Boolean isSelected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reserve);
+
+        getSeatsData getSeatsData = new getSeatsData();
+        getSeatsData.execute();
+    }
+
+    @Override
+    public void onClick(final View v) {
+        if(isSelected == false){
+            if((int) v.getTag() == STATUS_AVAILABLE){
+                selectedIds = selectedIds + v.getId() + ", ";
+                v.setBackgroundResource(R.drawable.ic_seats_selected);
+                isSelected = true;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("예약 확인");
+                builder.setMessage("Seat-" + v.getId() + " 예약 하시겠습니까");
+
+                builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "예약되었습니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ReserveActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+                builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "예약취소되었습니다.", Toast.LENGTH_SHORT).show();
+                        selectedIds = selectedIds.replace(+v.getId() + ",", "");
+                        v.setBackgroundResource(R.drawable.ic_seats_available);
+                        isSelected = false;
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            } else if((int) v.getTag() == STATUS_BOOKED){
+                Toast.makeText(this, "Seat " + v.getId() + " 예약되어있습니다.", Toast.LENGTH_SHORT).show();
+            } else if ((int) v.getTag() == STATUS_RESERVED) {
+                Toast.makeText(this, "Seat " + v.getId() + "은 이용이 불가합니다.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            if((int) v.getTag() == STATUS_AVAILABLE){
+                if(selectedIds.contains(v.getId() + ",")){
+                    selectedIds = selectedIds.replace(+v.getId() + ",", "");
+                    v.setBackgroundResource(R.drawable.ic_seats_available);
+                    isSelected = false;
+                }
+            }
+        }
+    }
+
+    public void drawSeats(String s){
+        layout = findViewById(R.id.layoutSeat);
+        seats = "/" + seats;
+
+        LinearLayout layoutSeat = new LinearLayout(ReserveActivity.this);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutSeat.setOrientation(LinearLayout.VERTICAL);
+        layoutSeat.setLayoutParams(params);
+        layoutSeat.setPadding(8*seatGaping,8*seatGaping, 8*seatGaping, 8*seatGaping);
+        layout.addView(layoutSeat);
+
+        LinearLayout layout = null;
+        int count = 0;
+
+        for (int index = 0; index < seats.length(); index++) {
+            if (seats.charAt(index) == '/') {
+                layout = new LinearLayout(ReserveActivity.this);
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+                layoutSeat.addView(layout);
+            } else if (seats.charAt(index) == '0' && s.charAt(count) == '0') {
+                count++;
+                TextView view = new TextView(ReserveActivity.this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count);
+                view.setGravity(Gravity.CENTER);
+                view.setBackgroundResource(R.drawable.ic_seats_available);
+                view.setText(count + "");
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                view.setTextColor(Color.BLACK);
+                view.setTag(STATUS_AVAILABLE);
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(ReserveActivity.this);
+            } else if (seats.charAt(index) == '0' && s.charAt(count) == '1') {
+                count++;
+                TextView view = new TextView(ReserveActivity.this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count);
+                view.setGravity(Gravity.CENTER);
+                view.setBackgroundResource(R.drawable.ic_seats_booked);
+                view.setTextColor(Color.WHITE);
+                view.setTag(STATUS_BOOKED);
+                view.setText(count + "");
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(ReserveActivity.this);
+            } else if (seats.charAt(index) == '0' && s.charAt(count) == '2') {
+                count++;
+                TextView view = new TextView(ReserveActivity.this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setPadding(0, 0, 0, 2 * seatGaping);
+                view.setId(count);
+                view.setGravity(Gravity.CENTER);
+                view.setBackgroundResource(R.drawable.ic_seats_reserved);
+                view.setText(count + "");
+                view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                view.setTextColor(Color.WHITE);
+                view.setTag(STATUS_RESERVED);
+                layout.addView(view);
+                seatViewList.add(view);
+                view.setOnClickListener(ReserveActivity.this);
+            } else if (seats.charAt(index) == '_') {
+                TextView view = new TextView(ReserveActivity.this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                view.setLayoutParams(layoutParams);
+                view.setBackgroundColor(Color.TRANSPARENT);
+                view.setText("");
+                layout.addView(view);
+            }
+        }
+    }
+
+    public class getSeatsData extends AsyncTask<String, Void, String>{
+        ProgressDialog progressDialog;
+        String errorString;
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(ReserveActivity.this, "Please wait...", null, true, true);
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String server_url = "http://13.124.28.135/getSeatsData.php";
+
+            try{
+                URL url = new URL(server_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+
+                InputStream inputStream;
+                if(responseStatusCode == httpURLConnection.HTTP_OK){
+                    inputStream = httpURLConnection.getInputStream();
+                }else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+            }catch (Exception e){
+                errorString = e.toString();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            progressDialog.dismiss();
+            drawSeats(s);
+        }
     }
 }
