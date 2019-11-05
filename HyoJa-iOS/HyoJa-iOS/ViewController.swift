1ac7aa4318c434be96c1d7d447d9205656b2f01f
eomjeongho <eomjeongho1@naver.com>
2019-11-05 10:08:32 +0900

    로그인 구현

--- HyoJa-iOS/HyoJa-iOS/ViewController.swift
+++ HyoJa-iOS/HyoJa-iOS/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CryptoSwift
 
 class ViewController: UIViewController {
 
@@ -15,18 +16,70 @@ class ViewController: UIViewController {
     @IBOutlet var PW: UITextField!
     @IBOutlet var LoginButton: UIButton!
     
+    var isLogin: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         ID.layer.cornerRadius = 10
         PW.layer.cornerRadius = 10
         LoginButton.layer.cornerRadius = 10
     }
 
     @IBAction func touchUpLoginButton(_ sender: UIButton){
-        let mainPage = self.storyboard?.instantiateViewController(identifier: "Main")
-        mainPage?.modalPresentationStyle = .fullScreen
-        self.present(mainPage!, animated: true, completion: nil)
-    }
-}
+        let key = "################################"
+        let iv = "################"
+        var cipher = ""
+        do{
+            let aes = try! AES(key: key.bytes, blockMode: CBC(iv: iv.bytes) , padding: .pkcs5)
+            cipher = try aes.encrypt(Array(PW.text!.utf8)).toBase64()!
+        }catch{}
 
+        data_request("http://13.124.28.135/check.php", id: ID.text!, pw: cipher)
+    }
+    
+    func data_request(_ url:String, id:String, pw:String)
+    {
+        let url:NSURL = NSURL(string: url)!
+        let session = URLSession.shared
+
+        var isLogin: Bool! = false
+        var done: Bool! = false
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+
+        let paramString = "id="+id+"&pw="+pw
+        request.httpBody = paramString.data(using: String.Encoding.utf8)
+
+        let task = session.dataTask(with: request as URLRequest) {
+            (
+            data, response, error) in
+
+            guard let _:NSData = data as NSData?, let _:URLResponse = response, error == nil else {
+                 print(error?.localizedDescription ?? "No data")
+                return
+            }
+
+            if var dataString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            {
+                if(dataString as String == "Success"){
+                    isLogin = true
+                }
+                done = true
+            }
+        }
+        
+        task.resume()
+        
+        repeat {
+            RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
+        } while !done
+        
+        if(isLogin){
+            let mainPage = self.storyboard?.instantiateViewController(identifier: "Main")
+            mainPage?.modalPresentationStyle = .fullScreen
+            self.present(mainPage!, animated: true, completion: nil)
+        }
+    }
+
+}
